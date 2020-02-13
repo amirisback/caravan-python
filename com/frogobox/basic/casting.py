e5ec2b3e81850a5187edd0351557246931159556
amirisback <faisalamircs@gmail.com>
2020-02-13 18:58:24 +0700

    add: casting and number

---
+++ com/frogobox/basic/casting.py
@@ -0,0 +1,45 @@
+#
+# Created by Faisal Amir
+# FrogoBox Inc License
+# -----------------------------------------
+# Learning-Python
+# Copyright (C) 13/02/2020.      
+# All rights reserved
+# -----------------------------------------
+# Name     : Muhammad Faisal Amir
+# E-mail   : faisalamircs@gmail.com
+# Github   : github.com/amirisback
+# LinkedIn : linkedin.com/in/faisalamircs
+# -----------------------------------------
+# FrogoBox Software Industries
+# 
+# /
+
+# integer
+xInt = int(1)   # x will be 1
+yInt = int(2.8) # y will be 2
+zInt = int("3") # z will be 3
+
+# float
+xFloat = float(1)     # x will be 1.0
+yFloat = float(2.8)   # y will be 2.8
+zFloat = float("3")   # z will be 3.0
+wFloat = float("4.2") # w will be 4.2
+
+# string
+xString = str("s1") # x will be 's1'
+yString = str(2)    # y will be '2'
+zString = str(3.0)  # z will be '3.0'
+
+print(xInt)
+print(yInt)
+print(zInt)
+
+print(xFloat)
+print(yFloat)
+print(zFloat)
+print(wFloat)
+
+print(xString)
+print(yString)
+print(zString)
---
+++ com/frogobox/basic/number.py
@@ -0,0 +1,41 @@
+#
+# Created by Faisal Amir
+# FrogoBox Inc License
+# -----------------------------------------
+# Learning-Python
+# Copyright (C) 13/02/2020.      
+# All rights reserved
+# -----------------------------------------
+# Name     : Muhammad Faisal Amir
+# E-mail   : faisalamircs@gmail.com
+# Github   : github.com/amirisback
+# LinkedIn : linkedin.com/in/faisalamircs
+# -----------------------------------------
+# FrogoBox Software Industries
+# 
+# /
+
+import random
+
+print(random.randrange(1,10))
+x = 1 # int
+y = 2.8 # float
+z = 1j # complex
+
+#convert from int to float:
+a = float(x)
+
+#convert from float to int:
+b = int(y)
+
+#convert from int to complex:
+c = complex(x)
+
+print(a)
+print(b)
+print(c)
+
+print(type(a))
+print(type(b))
+print(type(c))
+ 
